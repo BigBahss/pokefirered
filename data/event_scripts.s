352cde32235db4bbddf130fa91504c1b7b758236
Tyler Dennis <tdennis4496@gmail.com>
2020-08-16 23:58:01 -0400

    Rename mt silver 1f&2f to 1f_g2&2f_g2, and 1f_v2 to 1f

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1375,11 +1375,11 @@ Text_TestMsg:: @ 81ACD45
 	.include "data/maps/MtSilver_PokemonCenter_2F/scripts.inc"
 	.include "data/maps/MtSilver_PokemonCenter_2F/text.inc"
 
+	.include "data/maps/MtSilver_1F_G2/scripts.inc"
+	.include "data/maps/MtSilver_1F_G2/text.inc"
+
+	.include "data/maps/MtSilver_2F_G2/scripts.inc"
+	.include "data/maps/MtSilver_2F_G2/text.inc"
+
 	.include "data/maps/MtSilver_1F/scripts.inc"
 	.include "data/maps/MtSilver_1F/text.inc"
-
-	.include "data/maps/MtSilver_2F/scripts.inc"
-	.include "data/maps/MtSilver_2F/text.inc"
-
-	.include "data/maps/MtSilver_1F_V2/scripts.inc"
-	.include "data/maps/MtSilver_1F_V2/text.inc"
--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -133,9 +133,9 @@
 #define MAP_SIX_ISLAND_PATTERN_BUSH              (121 | (1 << 8))
 #define MAP_SIX_ISLAND_ALTERING_CAVE             (122 | (1 << 8))
 #define MAP_MT_SILVER_EXTERIOR                   (123 | (1 << 8))
-#define MAP_MT_SILVER_1F                         (124 | (1 << 8))
-#define MAP_MT_SILVER_2F                         (125 | (1 << 8))
-#define MAP_MT_SILVER_1F_V2                      (126 | (1 << 8))
+#define MAP_MT_SILVER_1F_G2                      (124 | (1 << 8))
+#define MAP_MT_SILVER_2F_G2                      (125 | (1 << 8))
+#define MAP_MT_SILVER_1F                         (126 | (1 << 8))
 
 // Map Group 2
 #define MAP_NAVEL_ROCK_EXTERIOR                       (0 | (2 << 8))
