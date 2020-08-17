065d18b8ac85a7fa33825c635a74deebdb40cca0
Tyler Dennis <tdennis4496@gmail.com>
2020-08-17 06:07:40 -0400

    Add collision to mt silver, remove old versions of maps

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1375,12 +1375,6 @@ Text_TestMsg:: @ 81ACD45
 	.include "data/maps/MtSilver_PokemonCenter_2F/scripts.inc"
 	.include "data/maps/MtSilver_PokemonCenter_2F/text.inc"
 
-	.include "data/maps/MtSilver_1F_G2/scripts.inc"
-	.include "data/maps/MtSilver_1F_G2/text.inc"
-
-	.include "data/maps/MtSilver_2F_G2/scripts.inc"
-	.include "data/maps/MtSilver_2F_G2/text.inc"
-
 	.include "data/maps/MtSilver_1F/scripts.inc"
 	.include "data/maps/MtSilver_1F/text.inc"
 
