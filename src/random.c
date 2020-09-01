a58e97ea9a4ba3dc1871831370c09d9236a9517b
BigBahss <tdennis4496@gmail.com>
2020-09-01 00:38:17 -0400

    Import random system from pokeemerald

--- src/random.c
+++ src/random.c
@@ -1,17 +1,28 @@
 #include "global.h"
+#include "random.h"
 
-// The number 1103515245 comes from the example implementation
-// of rand and srand in the ISO C standard.
-
+// IWRAM common
 u32 gRngValue;
+u32 gRng2Value;
 
 u16 Random(void)
 {
-    gRngValue = 1103515245 * gRngValue + 24691;
+    gRngValue = ISO_RANDOMIZE1(gRngValue);
     return gRngValue >> 16;
 }
 
 void SeedRng(u16 seed)
 {
     gRngValue = seed;
+}
+
+void SeedRng2(u16 seed)
+{
+    gRng2Value = seed;
+}
+
+u16 Random2(void)
+{
+    gRng2Value = ISO_RANDOMIZE1(gRng2Value);
+    return gRng2Value >> 16;
 }
